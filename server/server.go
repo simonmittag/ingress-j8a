d687a4a2b375efa1fa7a5d5c3cacaa5f3705719a
simon.mittag <simonmittag@gmail.com>
2023-05-13 15:45:19 +1000

    config maps and services fetched separately

--- server/server.go
+++ server/server.go
@@ -3,30 +3,37 @@ package server
 import (
 	"context"
 	"fmt"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"strconv"
 	"time"
 )
 
 const Version = "v0.1"
 
 type Server struct {
-	Version string
-	Kube    *kubernetes.Clientset
+	Version          string
+	Kube             *kubernetes.Clientset
+	KubeVersionMajor int
+	KubeVersionMinor int
 }
 
 func NewServer() *Server {
 	return &Server{
-		Version: Version,
-		Kube:    nil,
+		Version:          Version,
+		Kube:             nil,
+		KubeVersionMajor: 0,
+		KubeVersionMinor: 0,
 	}
 }
 
 func (s *Server) Bootstrap() {
 	s.authenticateToKube()
 	for {
-		s.printPods()
+		s.printObjects()
 	}
 }
 
@@ -41,14 +48,36 @@ func (s *Server) authenticateToKube() {
 		s.panic(err)
 	}
 	s.Kube = clientset
+	s.detectKubeVersion(config)
 }
 
-func (s *Server) printPods() {
-	pods, _ := s.Kube.CoreV1().Pods("").List(
+func (s *Server) detectKubeVersion(config *rest.Config) {
+	dc, _ := discovery.NewDiscoveryClientForConfig(config)
+	vi, _ := dc.ServerVersion()
+	s.KubeVersionMajor, _ = strconv.Atoi(vi.Major)
+	s.KubeVersionMinor, _ = strconv.Atoi(vi.Minor)
+}
+
+func (s *Server) printObjects() {
+	cm := s.fetchConfigMaps()
+	sv := s.fetchServices()
+	fmt.Printf("There are %d config maps in cluster running kube v%v.%v\n", len(cm.Items), s.KubeVersionMajor, s.KubeVersionMinor)
+	fmt.Printf("There are %d services in cluster running kube v%v.%v\n", len(sv.Items), s.KubeVersionMajor, s.KubeVersionMinor)
+	time.Sleep(time.Second * 3)
+}
+
+func (s *Server) fetchServices() *v1.ServiceList {
+	sv, _ := s.Kube.CoreV1().Services("").List(
 		context.TODO(),
 		metav1.ListOptions{})
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-	time.Sleep(time.Second * 3)
+	return sv
+}
+
+func (s *Server) fetchConfigMaps() *v1.ConfigMapList {
+	cml, _ := s.Kube.CoreV1().ConfigMaps("").List(
+		context.TODO(),
+		metav1.ListOptions{})
+	return cml
 }
 
 func (s *Server) panic(e error) {
