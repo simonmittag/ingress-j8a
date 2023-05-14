f444fa5df878a77601b76242b3e0d4eca62ce1b2
simon.mittag <simonmittag@gmail.com>
2023-05-14 14:21:32 +1000

    added ingress resource fetching

--- server/server.go
+++ server/server.go
@@ -2,12 +2,17 @@ package server
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	v1 "k8s.io/api/core/v1"
+	nv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
 	"strconv"
 	"time"
 )
@@ -31,13 +36,34 @@ func NewServer() *Server {
 }
 
 func (s *Server) Bootstrap() {
-	s.authenticateToKube()
+	s.authenticateToKubeInternal()
+	//s.authenticateToKubeExternal()
 	for {
 		s.printObjects()
 	}
 }
 
-func (s *Server) authenticateToKube() {
+func (s *Server) authenticateToKubeExternal() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "~/.kube/config", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, _ := kubernetes.NewForConfig(config)
+	s.Kube = clientset
+}
+
+func (s *Server) authenticateToKubeInternal() {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		s.panic(err)
@@ -61,8 +87,12 @@ func (s *Server) detectKubeVersion(config *rest.Config) {
 func (s *Server) printObjects() {
 	cm := s.fetchConfigMaps()
 	sv := s.fetchServices()
+	sl := s.fetchSecrets()
+	il := s.fetchIngress()
 	fmt.Printf("There are %d config maps in cluster running kube v%v.%v\n", len(cm.Items), s.KubeVersionMajor, s.KubeVersionMinor)
 	fmt.Printf("There are %d services in cluster running kube v%v.%v\n", len(sv.Items), s.KubeVersionMajor, s.KubeVersionMinor)
+	fmt.Printf("There are %d secrets in cluster running kube v%v.%v\n", len(sl.Items), s.KubeVersionMajor, s.KubeVersionMinor)
+	fmt.Printf("There are %d ingress in cluster running kube v%v.%v\n", len(il.Items), s.KubeVersionMajor, s.KubeVersionMinor)
 	time.Sleep(time.Second * 3)
 }
 
@@ -78,6 +108,21 @@ func (s *Server) fetchConfigMaps() *v1.ConfigMapList {
 		context.TODO(),
 		metav1.ListOptions{})
 	return cml
+}
+
+func (s *Server) fetchSecrets() *v1.SecretList {
+	sl, _ := s.Kube.CoreV1().Secrets("").List(
+		context.TODO(),
+		metav1.ListOptions{})
+	return sl
+}
+
+func (s *Server) fetchIngress() *nv1.IngressList {
+	il, e := s.Kube.NetworkingV1().Ingresses("").List(
+		context.TODO(),
+		metav1.ListOptions{})
+	fmt.Printf("%v", e)
+	return il
 }
 
 func (s *Server) panic(e error) {
