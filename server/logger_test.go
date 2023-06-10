1db871886043748b423304066aba7a5ec41e2c94
simon.mittag <simonmittag@gmail.com>
2023-06-10 17:43:46 +1000

    unit tests

--- server/logger_test.go
+++ server/logger_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestLogger(t *testing.T) {
 	l := NewKLoggerWrapper()
 	l.Infof("blah %v", "blah")
+	l.Info("blah")
 	l.Errorf("blah %v", "blah")
-	l.Fatalf("blah %v", "blah")
 }
